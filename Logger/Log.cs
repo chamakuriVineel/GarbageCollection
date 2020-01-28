2dd64df3847e41c4fbc6f9e448d35b84c3bac883
Vineel Chamakuri <Vineel_Chamakuri@epam.com>
2020-01-28 15:03:36 +0530

    Add project files.

---
+++ Logger/Log.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Logger
+{
+    
+    public class Log: IDisposable
+    {
+        //managed resource reference variable;
+        private String filePath;
+        //unmanged object reference variable;
+        private StreamWriter streamWriter;
+        public Log(String filePath)
+        {
+            this.filePath = filePath;
+            streamWriter = new StreamWriter(filePath);
+        }
+        public bool logIt(String message)
+        {
+            try
+            {
+                streamWriter.Flush();
+                streamWriter.WriteLine(message);
+                Dispose();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString()+ "Can't write the message to the log file");
+                return false;
+            }
+            return true;
+        }
+        public void Dispose()
+        {
+            DisposeHelper(true);
+            GC.SuppressFinalize(this);
+        }
+        protected virtual void DisposeHelper(bool disposing)
+        {
+            Console.WriteLine("DisposeHelper called with "+disposing+" argument");
+            if (disposing)
+            {
+                Console.WriteLine("managed resources are disposed");
+            }
+            streamWriter.Close();
+            Console.WriteLine("unmanaged resources are disposed"); 
+        }
+        ~Log()
+        {
+            Console.WriteLine("Call from the finalize method");
+            DisposeHelper(false);
+        }
+
+        
+    }
+}
---
+++ MyApp/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Logger;
+using System.IO;
+
+namespace MyApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String filePath = "C:\\Users\\Vineel_Chamakuri\\Documents\\DataBaseLog.txt";
+            Log Mylog = new Log(filePath);
+            String message = "Hi!,This is vineel chamakuri ";
+            Mylog.logIt(message);
+            Mylog = new Log("sample");
+            GC.Collect();
+            Console.WriteLine("Press Any key to run using block");
+            Console.ReadLine();
+            StreamReader streamReader;
+            using (streamReader = new StreamReader(filePath))
+            {
+                String data = streamReader.ReadLine();
+                Console.WriteLine("Accesing streamReader insideside the using block.....");
+                Console.WriteLine("The data from the file: " + filePath + " is\n" + data);
+            }
+            Console.WriteLine("Accesing streamReader outside the using block.....");
+            try
+            {
+                Console.WriteLine(streamReader.ReadLine());
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("ERROR Occured\n" + e.ToString());
+            }
+            Console.ReadLine();
+
+
+        }
+    }
+}
